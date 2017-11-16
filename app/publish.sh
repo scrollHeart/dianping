8b9802133809c7452a7a9161d47b59aec7a123be
GeekerHua <geekerhua@sina.com>
2017-11-16 11:20:44 +0800

    update dockerfile

--- app/publish.sh
+++ app/publish.sh
@@ -2,6 +2,3 @@
 
 docker build -t myflask .
 docker run -d --name dianping_flask -p 80:80 --link=dianping_mysql:db myflask
-
-sudo docker build -t reg.docker.alibaba-inc.com/yousang/price_inspector:v1.0 ./docker/
-sudo docker push reg.docker.alibaba-inc.com/yousang/price_inspector:v1.0
